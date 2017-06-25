2d96d9cc2c10387b2965fda320a79d3658cef1b7
JrKaiHu <JrKaiHu@users.noreply.github.com>
2017-06-25 14:27:19 +0800

    Refactor the source code
    Add new method to add new row to table in MainActivity

--- app/src/main/java/com/example/arieshu/sqliteexample/MainActivity.java
+++ app/src/main/java/com/example/arieshu/sqliteexample/MainActivity.java
@@ -45,50 +45,52 @@ public class MainActivity extends AppCompatActivity {
 
         int nCnt = m_StuList.size();
 
-//        for ( int i = 0 ; i < nCnt ; i++ ) {
-//            //Log.i("Test", m_StuList.get(i).m_stdName);
-//        }
-
-        int sizeInDP = 1;
-        int nTableCol = 4;
         int nTableRow = m_StuList.size();
 
         for( int i = 0 ; i < nTableRow ; i++ )
         {
-            TableRow tableRow = new TableRow(this);
-            for( int j = 0; j < nTableCol ; j++ )
-            {
-                //tv用于显示
-                android.widget.TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT);
-
-                int marginInDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, sizeInDP, getResources().getDisplayMetrics());
-
-                int marginInDpTop    = marginInDp;
-                int marginInDpLeft   = (j==0)?marginInDp*2:marginInDp;
-                int marginInDpRight  = (j==(nTableCol-1))?marginInDp*2:marginInDp;
-                int marginInDpBottom = (i==(nTableRow-1))?marginInDp*2:marginInDp;
-
-                layoutParams.setMargins(marginInDpLeft, marginInDpTop, marginInDpRight, marginInDpBottom);
-
-                TextView tv = new TextView(this);
-                //tv.setText("("+col+","+row+")");
-                tv.setBackgroundResource(R.color.tableItem);
-                tv.setTypeface(null, Typeface.BOLD);
-                tv.setTextSize(20);
-                tv.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                tv.setWidth(0);
-
-                if(j == 0) tv.setText(String.valueOf(m_StuList.get(i).m_nID));
-                else if(j == 1) tv.setText(m_StuList.get(i).m_stdName);
-                else if(j == 2) tv.setText(m_StuList.get(i).m_strSex);
-                else tv.setText(String.valueOf(m_StuList.get(i).m_nScore));
-
-                tableRow.setBackgroundResource(R.color.tableBackground);
-                tableRow.addView(tv, layoutParams);
-            }
-            // Add table row to table layout
-            m_TableRow.addView(tableRow, new TableLayout.LayoutParams(FP, WC));
+            addSingleRowToTable(m_StuList.get(i));
         }
+    }
+
+    private void addSingleRowToTable(StudentProfile stuPro) {
+
+        int nTableCol = 4;
+        int sizeInDP = 1;
+
+        TableRow tableRow = new TableRow(this);
+        for( int j = 0; j < nTableCol ; j++ )
+        {
+            //tv用于显示
+            android.widget.TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT);
+
+            int marginInDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, sizeInDP, getResources().getDisplayMetrics());
+
+            int marginInDpTop    = 0;
+            int marginInDpLeft   = (j==0)?marginInDp*2:marginInDp;
+            int marginInDpRight  = (j==(nTableCol-1))?marginInDp*2:marginInDp;
+            int marginInDpBottom = marginInDp*2;
+
+            layoutParams.setMargins(marginInDpLeft, marginInDpTop, marginInDpRight, marginInDpBottom);
+
+            TextView tv = new TextView(this);
+            //tv.setText("("+col+","+row+")");
+            tv.setBackgroundResource(R.color.tableItem);
+            tv.setTypeface(null, Typeface.BOLD);
+            tv.setTextSize(20);
+            tv.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+            tv.setWidth(0);
+
+            if(j == 0) tv.setText(String.valueOf(stuPro.m_nID));
+            else if(j == 1) tv.setText(stuPro.m_stdName);
+            else if(j == 2) tv.setText(stuPro.m_strSex);
+            else tv.setText(String.valueOf(stuPro.m_nScore));
+
+            tableRow.setBackgroundResource(R.color.tableBackground);
+            tableRow.addView(tv, layoutParams);
+        }
+        // Add table row to table layout
+        m_TableRow.addView(tableRow, new TableLayout.LayoutParams(FP, WC));
     }
 
     private void processControllers() {
